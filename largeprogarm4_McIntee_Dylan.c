ca2c4f16a6df0abb33e6463cdd453d37b5026892
dylanmc1ntee <dylangmcintee@gmail.com>
2023-11-26 16:48:12 -0500

    add Comments

--- largeprogarm4_McIntee_Dylan.c
+++ largeprogarm4_McIntee_Dylan.c
@@ -7,6 +7,14 @@
 #include<string.h>
 #include<ctype.h>
 
+/* 
+Creates a structure named student_t  
+Inside the structure are 4 components
+~The name of the student
+~Their homework average
+~Their quiz average
+~Their exam average
+*/
 typedef struct{
 
     char name [30];
@@ -16,6 +24,9 @@ typedef struct{
 
 }student_t;
 
+/*
+These are the prototypes for all of the functions
+*/
 void clearBuffer();
 void addStudent(student_t s[], int *ptr);
 void removeStudent(student_t s[], int *ptr);
@@ -25,13 +36,13 @@ void printGrade(student_t s[], int *ptr);
 int greeting();
 void outputText(student_t s[], int *ptr);
 
-int main()
+int main() //start of main function
 {
-    student_t students[5];
+    student_t students[5]; //initializes structure
 
     int numStudents = 0;
 
-    int *ptrStudents = &numStudents;
+    int *ptrStudents = &numStudents; //points to numStudents
 
     int choice = 0;
 
@@ -39,45 +50,51 @@ int main()
 
     printf("----------------------------------------------------------------------------\n");
 
-    while(choice != 6)
+    /*
+    This part of the function repeats while the user has not chosen 6
+    */
+    while(choice != 6) 
     {
         choice = greeting();
 
-        if(choice == 1)
+        /*
+        Each of the if statements correspond to a button the user presses
+        */
+        if(choice == 1) //1
         {
             addStudent(students, ptrStudents);
 
             printf("----------------------------------------------------------------------------\n");
         }
-        else if(choice == 2)
+        else if(choice == 2) //2
         {
             removeStudent(students, ptrStudents);
 
             printf("----------------------------------------------------------------------------\n");
         }
-        else if(choice == 3)
+        else if(choice == 3) //3
         {
             updateGrade(students);
 
             printf("----------------------------------------------------------------------------\n");
         }
-        else if(choice == 4)
+        else if(choice == 4) //4
         {
             missingGrade(students, ptrStudents);
 
             printf("----------------------------------------------------------------------------\n");
         }
-        else if(choice == 5)
+        else if(choice == 5) //5
         {
             printGrade(students, ptrStudents);
 
             printf("----------------------------------------------------------------------------\n");
         }
-        else if(choice == 6)
+        else if(choice == 6) //6
         {
             printf("----------------------------------------------------------------------------\n");
         }
-        else
+        else //invalid input
         {
             printf("\nInvalid Response!\n");
             printf("----------------------------------------------------------------------------\n");
@@ -88,19 +105,25 @@ int main()
 
     printf("Goodbye...\n\n");
     
-    return 0;
+    return 0; //end of main
 }
 
+/*
+This clears the char buffer
+*/
 void clearBuffer()
 {
     while(getchar() != '\n');
 }
 
+/*
+This function adds 1 student to the gradebook
+*/
 void addStudent(student_t s[], int *ptr)
 {
     char name[30];
 
-    if(*ptr != 5)
+    if(*ptr != 5) //if gradebook is not full
     {
         printf("\nEnter the name of the student: ");
 
@@ -108,24 +131,27 @@ void addStudent(student_t s[], int *ptr)
 
         fgets(name, 30, stdin);
 
-        strcpy(s[*ptr].name, name);
+        strcpy(s[*ptr].name, name); //copys name
 
-        s[*ptr].homeworkAvg = 0.00;
+        s[*ptr].homeworkAvg = 0.00; //sets to 0.00
 
-        s[*ptr].quizAvg = 0.00;
+        s[*ptr].quizAvg = 0.00; //sets to 0.00
 
-        s[*ptr].examAvg = 0.00;
+        s[*ptr].examAvg = 0.00; //sets to 0.00
 
-        *ptr = *ptr + 1;
+        *ptr = *ptr + 1; //adds 1 to student counter
 
         printf("\nStudent Added!\n");
     }
-    else
+    else //if gradebook full
     {
         printf("\nYour GradeBook is full!\n");
     }
 }
 
+/*
+This function removes 1 student from the gradebook
+*/
 void removeStudent(student_t s[], int *ptr)
 {
     char name[30];
@@ -138,31 +164,34 @@ void removeStudent(student_t s[], int *ptr)
 
     fgets(name, 30, stdin);
 
-    for(int i = 0; i < 5; i++)
+    for(int i = 0; i < 5; i++) //repeat 5 times
     {
-        if(strcmp(s[i].name, name) == 0)
+        if(strcmp(s[i].name, name) == 0) //if names match
         {
-            for(int j = i; j < 5; j++)
+            for(int j = i; j < 5; j++) //removes
             {
                 s[j] = s[j + 1];
             }
 
-            found = 1;
+            found = 1; 
 
-            *ptr = *ptr - 1;
+            *ptr = *ptr - 1; //student counter -1
         }
     }
 
-    if(found == 1)
+    if(found == 1) //student removed
     {
         printf("\nStudent Removed!\n");
     }
-    else
+    else //student not removed
     {
         printf("\nStudent not Found!\n");
     }
 }
 
+/*
+This function updates the grades of 1 student
+*/
 void updateGrade(student_t s[])
 {
     char name[30];
@@ -175,20 +204,23 @@ void updateGrade(student_t s[])
 
     fgets(name, 30, stdin);
 
-    for(int i = 0; i < 5; i++)
+    for(int i = 0; i < 5; i++) //repeats 5 times
     {
-        if(strcmp(s[i].name, name) == 0)
+        if(strcmp(s[i].name, name) == 0) //if names match
         {
             found = i;
         }
     }
 
-    if(found == 6)
+    if(found == 6) //if student not found
     {
         printf("\nStudent not found!\n");
     }
-    else
+    else //if found
     {
+        /*
+        This else statement asks the user to input the new grades of the student
+        */
         printf("\nStudent found!\n");
 
         printf("\nEnter updated homework grade: ");
@@ -205,13 +237,16 @@ void updateGrade(student_t s[])
     }
 }
 
+/*
+This function prints the names of all the students who are missing grades
+*/
 void missingGrade(student_t s[], int *ptr)
 {
     printf("\nThe following students are missing grades: \n");
 
-    for(int i = 0; i < *ptr; i++)
+    for(int i = 0; i < *ptr; i++) //for number of students
     {
-        if(s[i].homeworkAvg == 0.00 || s[i].quizAvg == 0.00 || s[i].examAvg == 0.00)
+        if(s[i].homeworkAvg == 0.00 || s[i].quizAvg == 0.00 || s[i].examAvg == 0.00) //if something is missing
         {
             printf("\n");
 
@@ -220,13 +255,16 @@ void missingGrade(student_t s[], int *ptr)
     }
 }
 
+/*
+This function prints all the grades of every student in the gradebook
+*/
 void printGrade(student_t s[], int *ptr)
 {
     printf("----------------------------------------------------------------------------\n");
 
-    for(int i = 0; i < *ptr; i++)
+    for(int i = 0; i < *ptr; i++) //for number of students
     {
-        double weighted = (.5 * s[i].homeworkAvg);
+        double weighted = (.5 * s[i].homeworkAvg); //calculate weighted avg
 
         weighted += (.2 * s[i].quizAvg);
 
@@ -244,13 +282,16 @@ void printGrade(student_t s[], int *ptr)
 
         printf("\nWeighted Average: %.2lf\n", weighted);
 
-        if(i != (*ptr - 1))
+        if(i != (*ptr - 1)) //if not last student
         {
             printf("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n");
         }
     }
 }
 
+/*
+This function displays the user all the choices that they can make and returns their choice
+*/
 int greeting()
 {
     int choice;
@@ -272,13 +313,16 @@ int greeting()
     return choice;
 }
 
+/*
+This function prints all of the grades onto a text file named gradebook.txt
+*/
 void outputText(student_t s[], int *ptr)
 {
-    FILE *outp = fopen("gradebook.txt", "w");
+    FILE *outp = fopen("gradebook.txt", "w"); //create pointer
 
-    for(int i = 0; i < *ptr; i++)
+    for(int i = 0; i < *ptr; i++) //for number of students
     {
-        double weighted = (.5 * s[i].homeworkAvg);
+        double weighted = (.5 * s[i].homeworkAvg); //calculate weighted avg
 
         weighted += (.2 * s[i].quizAvg);
 
@@ -299,5 +343,5 @@ void outputText(student_t s[], int *ptr)
         fprintf(outp, "----------------------------------------------------------------------------\n");
     }
 
-    fclose(outp);
+    fclose(outp); //closes file
 }
